890ee96ff03a4fbc5f9c8ad41eea1f2088f0233d
SANG <SANG@AS4738>
2015-05-04 10:48:56 +0700

    set background color for the application

--- BTL-Calculator/src/Calculator.java
+++ BTL-Calculator/src/Calculator.java
@@ -1,4 +1,15 @@
+import java.awt.Color;
+
 
 public class Calculator {
 
 }
+
+
+public calf(String title) {
+	super(title);
+	addWindowListener(this);
+	addKeyListener(this);
+
+	cp = new CopyPaste();
+	setBackground(Color.orange);
