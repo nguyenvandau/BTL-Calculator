8e8838800f0072092740ee6b5ea0d384c725cbf5
sangnx <sangnx.setbku@gmail.com>
2015-05-04 09:39:11 +0700

    Update Calculator.java
    calf
    set background color for the application

--- BTL-Calculator/src/Calculator/Calculator.java
+++ BTL-Calculator/src/Calculator/Calculator.java
@@ -12,3 +12,15 @@ public class Calculator {
 	}
 
 }
+
+
+
+
+
+	public calf(String title) {
+		super(title);
+		addWindowListener(this);
+		addKeyListener(this);
+
+		cp = new CopyPaste();
+		setBackground(Color.orange);
