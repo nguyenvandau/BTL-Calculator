79aba16c3bcad53182b8f7873f83223e0a952756
Truong <Truong@HappyCoding>
2015-05-06 15:28:51 +0700

    Tao class Calf

--- BTL-Calculator/src/Calculator.java
+++ BTL-Calculator/src/Calculator.java
@@ -1,17 +1,6 @@
-import java.awt.Color;
-import java.awt.GridLayout;
+
 
 
 public class Calculator {
 
 }
-
-
-public calf(String title) {
-	super(title);
-	addWindowListener(this);
-	addKeyListener(this);
-
-	cp = new CopyPaste();
-	setBackground(Color.orange);
-	setLayout(new GridLayout(1, 1));
