b4ea6339c0ff23c088eaee8878a898b4d691c0f1
Mai Văn Trường <maivantruong26101993@gmail.com>
2015-05-09 00:12:05 +0700

    Tao giao dien cho may tinh

--- BTL-Calculator/src/CopyPaste.java
+++ BTL-Calculator/src/CopyPaste.java
@@ -1,7 +1,15 @@
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.Transferable;
 
-public class CopyPaste implements ClipboardOwner  {
-  
-  Clipboard clip;
-  String ctxt;
+public class CopyPaste implements ClipboardOwner {
+
+	Clipboard clip;
+	String ctxt;
+
+	@Override
+	public void lostOwnership(Clipboard clipboard, Transferable contents) {
+		
+	}
 
 }
