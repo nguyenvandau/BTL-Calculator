066ecd9aeedded34b24d520b907c825b0b77dc2e
Mai Văn Trường <maivantruong26101993@gmail.com>
2015-05-18 22:37:39 +0700

    Update class DuLieu

---
+++ BTL-Calculator/src/DuLieu.java
@@ -0,0 +1,19 @@
+public class DuLieu {
+
+	private String giaTri;
+	private int danhDau;
+
+	public void set(String giaTri, int danhDau) {
+		this.giaTri = giaTri;
+		this.danhDau = danhDau;
+	}
+
+	public String getGiaTri() {
+		return giaTri;
+	}
+
+	public int getDanhDau() {
+		return danhDau;
+	}
+
+}
