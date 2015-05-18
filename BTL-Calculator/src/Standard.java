8a24e16521021607d45a140e99d63eef5cbf9429
sangnx <sangnx.setbku@gmail.com>
2015-05-19 02:37:03 +0700

    declare variables
    tạo class Standard, lớp xử lý với các tính toán cơ bản.

---
+++ BTL-Calculator/src/Standard.java
@@ -0,0 +1,44 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Standard extends javax.swing.JFrame {
+
+    private XuLy xuLy = new XuLy();
+    private double ans = 0;
+    private double m = 0;
+    
+    private javax.swing.JButton Ans;
+    private javax.swing.JButton CE;
+    private javax.swing.JButton chia;
+    private javax.swing.JButton cong;
+    private javax.swing.JButton dauPhay;
+    private javax.swing.JTextArea hienThi;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton ketQua;
+    private javax.swing.JButton mCong;
+    private javax.swing.JButton mTru;
+    private javax.swing.JButton mc;
+    private javax.swing.JButton mr;
+    private javax.swing.JButton ms;
+    private javax.swing.JButton nhan;
+    private javax.swing.JButton number0;
+    private javax.swing.JButton number1;
+    private javax.swing.JButton number2;
+    private javax.swing.JButton number3;
+    private javax.swing.JButton number4;
+    private javax.swing.JButton number5;
+    private javax.swing.JButton number6;
+    private javax.swing.JButton number7;
+    private javax.swing.JButton number8;
+    private javax.swing.JButton number9;
+    private javax.swing.JMenuItem scientific;
+    private javax.swing.JButton tru;
+    private javax.swing.JMenu view;
+    private javax.swing.JButton xoa;
+    
+    
+
+}
