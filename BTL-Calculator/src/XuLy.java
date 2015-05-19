8d1dfd39c213a6f0103f094ae1dcfd35c318f6e7
Mai Văn Trường <maivantruong26101993@gmail.com>
2015-05-19 09:28:08 +0700

    Ham xoa va hienThi trong class XuLy

--- BTL-Calculator/src/XuLy.java
+++ BTL-Calculator/src/XuLy.java
@@ -1,6 +1,5 @@
 import java.util.Stack;
 
-
 public class XuLy {
 
 	private Stack<DuLieu> nhap = new Stack<DuLieu>();
@@ -9,38 +8,68 @@ public class XuLy {
 	private boolean ra = false;
 	private double ans = 0;
 	private double m = 0;
-	
+
 	public final int SO = 0; // So, dau phay
-	public final int UU_TIEN_1 = 1; 	//sin, cos
-	public final int UU_TIEN_2 = 2; 	// luy thua
-	public final int UU_TIEN_3 = 3;		// nhan chia
-	public final int UU_TIEN_4 = 4;		// cong tru
+	public final int UU_TIEN_1 = 1; // sin, cos
+	public final int UU_TIEN_2 = 2; // luy thua
+	public final int UU_TIEN_3 = 3; // nhan chia
+	public final int UU_TIEN_4 = 4; // cong tru
 	public final int MO_NGOAC = 5;
 	public final int DONG_NGOAC = 6;
 	public final int HANG_SO = -2;
 	public final int GIAI_THUA = -1;
-	
+
 	public void chon(boolean ra) {
 		this.ra = ra;
 	}
-	
+
 	public void setAns(double ans) {
 		this.ans = ans;
 	}
-	
+
 	public void setM(double m) {
 		this.m = m;
 	}
-	
-	public void xoaTatCa(){
+
+	public void xoaTatCa() {
 		nhap.clear();
 		bieuThuc.clear();
 		ketQua.clear();
 	}
-	
-	public void them(String s, int i){
+
+	public void them(String s, int i) {
 		DuLieu data = new DuLieu();
 		data.set(s, i);
 		nhap.push(data);
 	}
+
+	public void xoa() throws Exception {
+		if (nhap.empty()) {
+			throw new Exception();
+		} else
+			nhap.pop();
+	}
+
+	public String hienThi() throws Exception {
+		if (nhap.empty()) {
+			throw new Exception();
+		} else {
+			Stack<DuLieu> t = new Stack<DuLieu>();
+			String s = "";
+
+			while (nhap.empty() == false) {
+				t.push(nhap.pop());
+			}
+
+			while (t.empty() == false) {
+				s = s + t.peek().getGiaTri();
+				nhap.push(t.pop());
+			}
+			
+			return s;
+		}
+		
+	}
+	
+	
 }
