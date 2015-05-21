e9208ffd7b0f8de560296d00df7a2336d540d0b3
Mai Văn Trường <maivantruong26101993@gmail.com>
2015-05-21 20:23:14 +0700

    Ham main() cua class Standard

--- BTL-Calculator/src/Standard.java
+++ BTL-Calculator/src/Standard.java
@@ -503,4 +503,32 @@ public class Standard extends javax.swing.JFrame {
 		setVisible(false);
 		scientific.setVisible(true);
 	}
+	
+public static void main(String [] args) throws Exception {
+    	
+    	try {
+    		for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()){
+    			if ("Nimbus".equals(info.getName())){
+    				javax.swing.UIManager.setLookAndFeel(info.getClassName());
+    				break;
+    			}
+    		}
+    	} catch (ClassNotFoundException ex) {
+    		java.util.logging.Logger.getLogger(Standard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    	} catch (InstantiationException ex) {
+    		java.util.logging.Logger.getLogger(Standard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    	} catch (IllegalAccessException ex) {
+    		java.util.logging.Logger.getLogger(Standard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    	} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+    		java.util.logging.Logger.getLogger(Standard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    	}
+    	
+    	java.awt.EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new Standard().setVisible(true);
+			}
+		});
+    }
 }
